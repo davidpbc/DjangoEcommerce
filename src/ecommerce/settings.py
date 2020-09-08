3837693bc6de890b24232d4aa7b6729131bb0b31
David Padilla <david.pabc@gmail.com>
2020-09-08 18:20:05 -0500

    Secret key is test anyway

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xwdnlljw=73v_lbc&mxu%azpyyw3j^-bb&$ce#vdlkfc+wiztr'
+SECRET_KEY = 'this-is-a-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
